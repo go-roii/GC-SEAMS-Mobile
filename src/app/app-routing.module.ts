bbedb9409a1641cb6bc62f3f61df81b73a11b696
Mark Jason Margallo <jasonmarkollagram@gmail.com>
2021-12-01 15:54:50 +0800

    Added guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {AuthGuard} from './guards/auth.guard';
+import {AutologinGuard} from './guards/autologin.guard';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./login/login.page.module').then(m => m.LoginPageModule)
+    loadChildren: () => import('./login/login.page.module').then(m => m.LoginPageModule),
+    canLoad: [AutologinGuard]
   },
   {
     path: 'register',
-    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule),
+    canLoad: [AutologinGuard]
   },
   {
     path: 'homescreen',
-    loadChildren: () => import('./homescreen/tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./homescreen/tabs/tabs.module').then(m => m.TabsPageModule),
+    canLoad: [AuthGuard]
   },
   {
     path: 'scan-qr-modal',
-    loadChildren: () => import('./homescreen/scan-qr-modal/scan-qr-modal.module').then( m => m.ScanQrModalPageModule)
+    loadChildren: () => import('./homescreen/scan-qr-modal/scan-qr-modal.module').then( m => m.ScanQrModalPageModule),
+    canLoad: [AuthGuard]
   }
 ];
 @NgModule({
