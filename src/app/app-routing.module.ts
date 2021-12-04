964a4c8d00b95d1a109dda58ea2198ed2fdbbedc
Mark Jason Margallo <jasonmarkollagram@gmail.com>
2021-12-04 14:43:40 +0800

    Implemented draft of get invitations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,22 +7,18 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./login/login.page.module').then(m => m.LoginPageModule),
-    canLoad: [AutologinGuard]
   },
   {
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule),
-    canLoad: [AutologinGuard]
   },
   {
     path: 'homescreen',
     loadChildren: () => import('./homescreen/tabs/tabs.module').then(m => m.TabsPageModule),
-    canLoad: [AuthGuard]
   },
   {
     path: 'scan-qr-modal',
     loadChildren: () => import('./homescreen/scan-qr-modal/scan-qr-modal.module').then( m => m.ScanQrModalPageModule),
-    canLoad: [AuthGuard]
   }
 ];
 @NgModule({
--- src/app/homescreen/tab1/tab1.page.ts
+++ src/app/homescreen/tab1/tab1.page.ts
@@ -1,22 +1,65 @@
-import { Component } from '@angular/core';
+/* eslint-disable */
+import {Component, OnInit} from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { ScanQrModalPage } from '../scan-qr-modal/scan-qr-modal.page';
+import {Invitation} from "../../models/Invitation";
+import {DataService} from "../../services/data.service";
+import {UserService} from "../../services/user.service";
+import {HttpHeaders} from "@angular/common/http";
+import {RequestParams} from "../../models/RequestParams";
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  constructor(public modalController: ModalController) {}
+  invitations: Invitation[]=[];
+
+  constructor(public modalController: ModalController, private dataService: DataService, private userService: UserService) {}
 
   async presentModal() {
     const modal = await this.modalController.create({
       component: ScanQrModalPage,
       cssClass: 'my-custom-class'
     });
+
     return await modal.present();
+
+  }
+
+  getHttpOptions(){
+    const trimmedHeader=this.userService.getAuthHeader().split(':');
+    const httpOptions = {
+
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        Authorization: trimmedHeader[1]
+      })
+    };
+    return httpOptions;
+  }
+
+  fetchInvitations(){
+    const invitationParams=new RequestParams();
+    invitationParams.EndPoint='events/invitations'
+    invitationParams.requestType=5;
+    invitationParams.authToken=this.getHttpOptions();
+
+    this.dataService.httprequest(invitationParams)
+      .subscribe(async (data: Invitation[]) =>{
+        await this.setInvitations(data);
+        await console.log(this.invitations)
+      });
+  }
+
+  setInvitations(data: Invitation[]){
+    this.invitations=data;
+  }
+
+  ngOnInit(): void {
+    this.fetchInvitations();
   }
 
 }
